10be0c6bb16d887fb77d45fe2864f4e5549ace45
Matthias Bolte <matthias@tinkerforge.com>
2015-02-13 17:14:23 +0100

    vbnet: Use ReadLine instead of ReadKey
    Because ReadKey doesn't work with a pipe as stdin on the RED Brick.

--- software/examples/vbnet/ExampleThreshold.vb
+++ software/examples/vbnet/ExampleThreshold.vb
@@ -27,7 +27,7 @@ Module ExampleThreshold
         m.SetMoistureCallbackThreshold(">"C, 200, 0)
 
         System.Console.WriteLine("Press key to exit")
-        System.Console.ReadKey()
+        System.Console.ReadLine()
         ipcon.Disconnect()
     End Sub
 End Module
