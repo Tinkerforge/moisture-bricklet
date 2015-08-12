447bce4d8c6c76c5d07b2f2bc4a0964334af1c07
Matthias Bolte <matthias@tinkerforge.com>
2015-08-12 11:27:32 +0200

    Fix minor warnings in C/C++ examples

--- software/examples/c/example_simple.c
+++ software/examples/c/example_simple.c
@@ -7,7 +7,7 @@
 #define PORT 4223
 #define UID "XYZ" // Change to your UID
 
-int main() {
+int main(void) {
 	// Create IP connection
 	IPConnection ipcon;
 	ipcon_create(&ipcon);
@@ -19,7 +19,7 @@ int main() {
 	// Connect to brickd
 	if(ipcon_connect(&ipcon, HOST, PORT) < 0) {
 		fprintf(stderr, "Could not connect\n");
-		exit(1);
+		return 1;
 	}
 	// Don't use device before ipcon is connected
 
@@ -27,7 +27,7 @@ int main() {
 	uint16_t moisture_value;
 	if(moisture_get_moisture_value(&m, &moisture_value) < 0) {
 		fprintf(stderr, "Could not get moisture value, probably timeout\n");
-		exit(1);
+		return 1;
 	}
 
 	printf("Moisture Value: %d\n", moisture_value);
@@ -35,4 +35,5 @@ int main() {
 	printf("Press key to exit\n");
 	getchar();
 	ipcon_destroy(&ipcon); // Calls ipcon_disconnect internally
+	return 0;
 }
