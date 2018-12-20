d56a213217846bc93ed65e7b5b154dcaafd56f83
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:56:45 +0100

    rust: Reformat examples.

--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  moisture_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{ip_connection::IpConnection, moisture_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,24 +10,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     let m = MoistureBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Get threshold receivers with a debounce time of 1 second (1000ms).
-		m.set_debounce_period(1000);
+    // Get threshold receivers with a debounce time of 1 second (1000ms).
+    m.set_debounce_period(1000);
 
-     let moisture_reached_receiver = m.get_moisture_reached_callback_receiver();
+    let moisture_reached_receiver = m.get_moisture_reached_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `m` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for moisture_reached in moisture_reached_receiver {           
-                		println!("Moisture Value: {}", moisture_reached);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `m` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for moisture_reached in moisture_reached_receiver {
+            println!("Moisture Value: {}", moisture_reached);
+        }
+    });
 
-		// Configure threshold for moisture value "greater than 200".
-		m.set_moisture_callback_threshold('>', 200, 0);
+    // Configure threshold for moisture value "greater than 200".
+    m.set_moisture_callback_threshold('>', 200, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
