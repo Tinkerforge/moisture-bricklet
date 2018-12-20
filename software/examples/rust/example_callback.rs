bdc798d5d9888124ac449d17eea79ec3275d3301
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:37:35 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,5 +1,8 @@
-use std::{error::Error, io, thread};
-use tinkerforge::{ip_connection::IpConnection, moisture_bricklet::*};
+use std::{io, error::Error};
+use std::thread;
+use tinkerforge::{ip_connection::IpConnection, 
+                  moisture_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -10,23 +13,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let m = MoistureBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    let moisture_receiver = m.get_moisture_callback_receiver();
+     let moisture_receiver = m.get_moisture_callback_receiver();
 
-    // Spawn thread to handle received callback messages.
-    // This thread ends when the `m` object
-    // is dropped, so there is no need for manual cleanup.
-    thread::spawn(move || {
-        for moisture in moisture_receiver {
-            println!("Moisture Value: {}", moisture);
-        }
-    });
+        // Spawn thread to handle received callback messages. 
+        // This thread ends when the `m` object
+        // is dropped, so there is no need for manual cleanup.
+        thread::spawn(move || {
+            for moisture in moisture_receiver {           
+                		println!("Moisture Value: {}", moisture);
+            }
+        });
 
-    // Set period for moisture value receiver to 1s (1000ms).
-    // Note: The moisture value callback is only called every second
-    //       if the moisture value has changed since the last call!
-    m.set_moisture_callback_period(1000);
+		// Set period for moisture value receiver to 1s (1000ms).
+		// Note: The moisture value callback is only called every second
+		//       if the moisture value has changed since the last call!
+		m.set_moisture_callback_period(1000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,6 +1,8 @@
-use std::{error::Error, io};
+use std::{io, error::Error};
 
-use tinkerforge::{ip_connection::IpConnection, moisture_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, 
+                  moisture_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -11,11 +13,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let m = MoistureBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    // Get current moisture value.
-    let moisture = m.get_moisture_value().recv()?;
-    println!("Moisture Value: {}", moisture);
+		// Get current moisture value.
+let moisture = m.get_moisture_value().recv()?;
+		println!("Moisture Value: {}", moisture);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
